b40fca14921472888c5b921e38da20c099ea4469
varun dhand <varun@email.com>
2023-02-21 01:33:14 +0530

    project setup done

---
+++ main.js
@@ -0,0 +1,6 @@
+const APP_ID = "6dc260bde01045b398c27ed429bb1b0a"
+const TOKEN = "007eJxTYNCvUFnH8fXtm5ectl7XQv5yO4WKX9G+8HV/ooqI+/YgA04FBrOUZCMzg6SUVANDAxPTJGNLi2Qj89QUEyPLpCTDJINElzOfkxsCGRmiDINZGBkgEMRnYchNzMxjYAAA8oMeAw=="
+const CHANNEL = "main"
+
+const client = AgoraRTC.createClient({mode:'rtc',codec:'vp8'}) // making the client object--> interface that provides the local client with basic functionalities
+
